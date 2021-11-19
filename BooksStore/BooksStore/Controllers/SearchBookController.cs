aec3d74f6c9c97dc4bc32340c5fd4405e1749c6d
BEST-KOMP\Admin <rewers212@gmail.com>
2021-11-19 18:41:06 +0200

    fix

--- BooksStore/BooksStore/Controllers/SearchBookController.cs
+++ BooksStore/BooksStore/Controllers/SearchBookController.cs
@@ -1,11 +1,23 @@
-﻿using System;
+﻿using BooksStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.WebApi;
 
 namespace BooksStore.Controllers
 {
-    public class SearchBookController
+    public class SearchBookController : UmbracoApiController
     {
+        public ActionResult Index(BookModel model,string name,string genres)
+        {
+            var searchPageModel = new BookModel();
+
+            searchPageModel.Name = name;
+            searchPageModel.Genres = genres;
+
+            return null;
+        }
     }
 }
