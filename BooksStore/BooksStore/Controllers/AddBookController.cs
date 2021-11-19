0f70e3df6b7f2a4315cb61d762b905f5504b36eb
OMIS\Geiko_J <rewers212>
2021-11-19 15:21:21 +0200

    Umbraco

---
+++ BooksStore/BooksStore/Controllers/AddBookController.cs
@@ -0,0 +1,28 @@
+﻿using BooksStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+
+namespace BooksStore.Controllers
+{
+    public class AddBookController : UmbracoApiController
+    {
+        [HttpPost]
+        public void AddBook(BookModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return;
+            }
+            var memberservice = Services.MemberService;
+            var member = memberservice.CreateMemberWithIdentity(model.Name, model.Author, model.Genres, model.Rating);
+            member.SetValue("name", model.Name);
+            member.SetValue("author", model.Author);
+            member.SetValue("genres", model.Genres);
+            member.SetValue("rating", model.Rating);
+        }
+    }
+}
---
+++ BooksStore/BooksStore/Models/BookModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BooksStore.Models
+{
+    public class BookModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Genres { get; set; }
+        [Required]
+        public string Rating { get; set; }
+
+    }
+}
