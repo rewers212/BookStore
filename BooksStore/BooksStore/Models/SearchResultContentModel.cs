97f32c058da4dfa3b8acfaf09cbcd8e37d16531a
OMIS\Geiko_J <rewers212>
2021-11-23 16:38:17 +0200

    Search

---
+++ BooksStore/BooksStore/Models/SearchResultContentModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace BooksStore.Models
+{
+    public class SearchResultContentModel : PublishedContentWrapped
+    {
+        public SearchResultModel Results { get; private set; }
+
+        public SearchResultContentModel(IPublishedContent content, SearchResultModel results)
+            : base(content)
+        {
+            Results = results;
+        }
+    }
+}
